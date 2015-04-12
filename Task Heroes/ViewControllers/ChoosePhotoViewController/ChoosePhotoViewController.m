a27a46b6c8c7622b5007f22df4cc35018956f951
Andreea-Daniela Ene <ene_andreea@ymail.com>
2015-04-13 01:08:02 +0300

    Avatar image is refreshing after the update

--- Task Heroes/ViewControllers/ChoosePhotoViewController/ChoosePhotoViewController.m
+++ Task Heroes/ViewControllers/ChoosePhotoViewController/ChoosePhotoViewController.m
@@ -26,15 +26,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @synthesize imageView,choosePhotoBtn,takePhotoBtn;
 
@@ -57,47 +48,20 @@
 }
 
 - (IBAction)backButton:(id)sender {
-	//TableViewController *mvid=[[TableViewController alloc]initWithNibName:@"TableViewController" bundle:nil];
-	//mvid.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
-	//mvid.avatar.image = imageView.image; // this is how you do it
-	TableViewController *secondViewController = [[TableViewController alloc] init];
-	
-	//secondViewController.avatar.image = imageView.image; // Set the exposed property
-	//[secondViewController.avatar setImage:imageView.image];
-	
-	
-	//TableViewController *mvid=[[TableViewController alloc]initWithNibName:@"TableViewController" bundle:nil];
-	//NSLog(@"inainte de alocare: %@",secondViewController.avatar.image);
-	//secondViewController.set
-	//[secondViewController setNewImage:(imageView.image)];
-	
-//	NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-//	NSString *jpegFilePath = [NSString stringWithFormat:@"%@/Wplc.jpeg",docDir];
-//	NSData *data2 = [NSData dataWithData:UIImageJPEGRepresentation(imageView.image, 1.0f)];
-//	[data2 writeToFile:jpegFilePath atomically:YES];
 	
 	NSData *imageData = UIImageJPEGRepresentation(imageView.image, 1.0f); //convert image into .png format.
-	
-	
 	NSFileManager *fileManager = [NSFileManager defaultManager];//create instance of NSFileManager
-	
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES); //create an array and store result of our search for the documents directory in it
-	
 	NSString *documentsDirectory = [paths objectAtIndex:0]; //create NSString object, that holds our exact path to the documents directory
-	
-	NSString *fullPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"Default.jpg", imageView.image]]; //add our image to the path
+	NSString *fullPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"Default.jpg"]];//, imageView.image]]; //add our image to the path
 	if (imageView.image != nil) {
 		[fileManager createFileAtPath:fullPath contents:imageData attributes:nil]; //finally save the path (image)
-		//secondViewController.avatar.image=[UIImage imageNamed:@"Default.jpg"];
-		//[secondViewController.avatar setNeedsDisplay];
-		[self listFileAtPath:documentsDirectory];
+		//[self listFileAtPath:documentsDirectory];
 		NSLog(@"image saved");
 	}
-//	mvid.avatar.image = imageView.image; // this is how you do it
-//	NSLog(@"dupa alocare: %@",secondViewController.avatar.image);
-//	NSLog(@"Exista imaginea: %@",imageView.image);
 	[self dismissViewControllerAnimated:YES completion:nil];
-	[secondViewController setNewImage:(secondViewController.avatar.image)];
+}
+
 	
 	//---> DELETING <---///
 //	NSFileManager *fileMgr = [[NSFileManager alloc] init];
@@ -117,27 +81,25 @@
 //	 NSLog(@"Eroare");
 //	 
 // }
-}
+//}
 
-// List all files in Directory
--(NSArray *)listFileAtPath:(NSString *)path
-{
-	//-----> LIST ALL FILES <-----//
-	NSLog(@"LISTING ALL FILES FOUND");
-	
-	int count;
-	
-	NSArray *directoryContent = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:NULL];
-	for (count = 0; count < (int)[directoryContent count]; count++)
-	{
-		NSLog(@"File %d: %@", (count + 1), [directoryContent objectAtIndex:count]);
-	}
-	return directoryContent;
-	
-	
-}
-
-
+//// List all files in Directory
+//-(NSArray *)listFileAtPath:(NSString *)path
+//{
+//	//-----> LIST ALL FILES <-----//
+//	NSLog(@"LISTING ALL FILES FOUND");
+//	
+//	int count;
+//	
+//	NSArray *directoryContent = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:NULL];
+//	for (count = 0; count < (int)[directoryContent count]; count++)
+//	{
+//		NSLog(@"File %d: %@", (count + 1), [directoryContent objectAtIndex:count]);
+//	}
+//	return directoryContent;
+//	
+//	
+//}
 
 
 //-(void)storeImageNSDefaults{
@@ -147,8 +109,6 @@
 //	[[NSUserDefaults  standardUserDefaults] setObject:imgData forKey:@"image"];
 //	[[NSUserDefaults  standardUserDefaults]synchronize];
 //}
-
-
 
 
 @end
